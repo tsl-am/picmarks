3a698031820c808786dc023884e99a6fdc1ea4b2
Raghu Betina <raghu@starterleague.com>
2012-10-17 09:33:20 -0500

    Attempting to let users add new pictures to the list.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Picmarks::Application.routes.draw do
+  get "/pictures/new", :controller => 'pictures', :action => 'new'
   
   get "/pictures", :controller => 'pictures', :action => 'index'
 
-
   get "/pictures/:id", :controller => 'pictures', :action => 'show'
-  
-  # get "/pictures/1", :controller => 'pictures', :action => 'pic1'
-  # get "/pictures/2", :controller => 'pictures', :action => 'pic2'
-  
-  # Support /pictures/1
-  # Support /pictures/2
-  # Support /pictures/3, etc.
-  #
-  # I should see the correct picture
-  # with the correct title or caption for that picture
-  
+    
 end
